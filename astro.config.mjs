37fd50e29f9696793f4c84175469c94ba2410d89
JayeshVP24 <jayeshpotlabattini@gmail.com>
2023-04-02 14:05:52 +0530

    Integrations added: SEO automator, sitemap generator, robots.txt genrator, tailwindCSS, image optimizer, preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import image from "@astrojs/image";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), robotsTxt(), image(), preact()]
+});
