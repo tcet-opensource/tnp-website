c45b31844bfd6cffe3bbd7e727e61a529c925661
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-04-12 01:03:09 +0530

    Made responsive

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -3,6 +3,7 @@ import CountUp from "react-countup";
 import GridWrapper from "./GridWrapper";
 import { SwiperSlide, Swiper } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { useMediaQuery } from "react-responsive";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -10,12 +11,17 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
 const HeroSection = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 660px)" });
+  const isTablet = useMediaQuery({ query: "(max-width: 1000px)" });
+
   return (
     <>
-      <div className="flex">
-        <div className="w-[50%] p-10">
+      {/* Text of the HeroSection */}
+      <div className="flex" style={{ display: isMobile ? "grid" : "flex" }}>
+        <div className="p-10" style={{ width: isMobile ? "100%" : "50%" }}>
           <button className="flex bg-[#F2F4F7] p-[.5rem] rounded-[19px]">
             <div className="px-3">Visit Our Guidelines</div>
+            {/* Svg for arrow in button */}
             <svg
               width="24"
               height="24"
@@ -33,13 +39,22 @@ const HeroSection = () => {
               />
             </svg>
           </button>
-          <div className="text-[42px] bold text-title">
+          <div
+            className="bold text-title"
+            style={{ fontSize: isTablet ? "2rem" : "2.4rem" }}
+          >
             "Unlock Your Career Potential with Training And Placement Cell -
             Where Dreams Meet Opportunities!"
           </div>
         </div>
-        <div className="w-[50%] p-10">
-          <div className="text-[18px] text-[#475467] leading-[28px] tracking-wider">
+        <div
+          className="w-[50%] p-10 justify-between flex flex-col"
+          style={{ width: isMobile ? "100%" : "50%" }}
+        >
+          <div
+            className="text-[#475467] leading-[28px] tracking-wider"
+            style={{ fontSize: isTablet ? "1rem" : "18px" }}
+          >
             The Training and Placement department serves as a bridge between the
             academic environment and the industry by establishing connections
             with various companies and organizations. The ultimate aim of the
@@ -55,8 +70,10 @@ const HeroSection = () => {
           </button>
         </div>
       </div>
-      <div className="flex justify-evenly">
-        <div className="max-w-[40%] ml-6">
+
+      {/* Numbers and text inside it */}
+      <div className="justify-evenly" style={{ display: isTablet ? "grid" : "flex" }}>
+        <div className="ml-6" style={{ maxWidth: isTablet ? "100%" : "40%" }}>
           <GridWrapper columns={2}>
             <div
               className="max-w-[180px] p-4 m-3 rounded-2xl"
@@ -102,28 +119,45 @@ const HeroSection = () => {
             </div>
           </GridWrapper>
         </div>
+
+        {/* Swiper/ Carousel */}
         <Swiper
-          // install Swiper modules
           modules={[Navigation, Pagination, Scrollbar, A11y]}
           spaceBetween={50}
           navigation
           pagination={{ clickable: true }}
-          style={{ maxWidth: "50%", margin: "1rem" }}
+          style={{ maxWidth: isTablet ? "100%" : "40%", margin: "1rem" }}
           scrollbar={{ draggable: true }}
           onSwiper={(swiper) => console.log(swiper)}
           onSlideChange={() => console.log("slide change")}
         >
-        <SwiperSlide>
-          <img className="w-full" src="/images/swiperImage1.png" alt="image" />
-        </SwiperSlide>
           <SwiperSlide>
-          <img className="w-full" src="/images/swiperImage1.png" alt="image" />
+            <img
+              className="w-full"
+              src="/images/swiperImage1.png"
+              alt="image"
+            />
           </SwiperSlide>
           <SwiperSlide>
-          <img className="w-full" src="/images/swiperImage1.png" alt="image" />
+            <img
+              className="w-full"
+              src="/images/swiperImage1.png"
+              alt="image"
+            />
           </SwiperSlide>
           <SwiperSlide>
-          <img className="w-full" src="/images/swiperImage1.png" alt="image" />
+            <img
+              className="w-full"
+              src="/images/swiperImage1.png"
+              alt="image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="w-full"
+              src="/images/swiperImage1.png"
+              alt="image"
+            />
           </SwiperSlide>
         </Swiper>
       </div>
