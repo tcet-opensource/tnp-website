ba9dd8707eb44ad1d83a03badb850a1d0a57c239
Shreyas30 <jshreyas12@gmail.com>
2023-04-11 17:48:20 +0530

    Changed Flex Direction from row to column in screen width medium

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='mt-4 px-4 py-2'>
-            <div id="homepage-footer" className="flex justify-around items-center font-sans flex-wrap">
+            <div id="homepage-footer" className="flex justify-around items-center font-sans flex-col md:flex-row">
 
                 {/* Map And Address */}
-                <div className="w-full sm:w-1/4 m-2">
+                <div className="w-full lg:w-1/2 xl:w-1/3 m-2">
                     <div>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3767.7457229439237!2d72.87241691481825!3d19.20630535285474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b0e57647569d%3A0xc0aec329c82d3555!2sThakur%20College%20of%20Engineering%20and%20Technology!5e0!3m2!1sen!2sin!4v1680794472903!5m2!1sen!2sin" style={{ border: 0, width: "100%", height: "27vh" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3767.7457229439237!2d72.87241691481825!3d19.20630535285474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b0e57647569d%3A0xc0aec329c82d3555!2sThakur%20College%20of%20Engineering%20and%20Technology!5e0!3m2!1sen!2sin!4v1680794472903!5m2!1sen!2sin" style={{ border: 0, width: "100%", height: "27vh" }} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                     <div className='mt-4 text-sm'>
                         <p className='text-justify' >Thakur College Of Engineering And Technology. A-Block, Thakur Educational Campus, Shyamnarayan Thakur Marg, Thakur Village, Kandivali(E). Mumbai - 400101.</p>
@@ -17,7 +17,7 @@ const Footer = () => {
                 </div>
 
                 {/* Contacts and CopyRight */}
-                <div className="w-full sm:w-auto mt-10 sm:mt-0 m-2">
+                <div className="w-full md:w-auto mt-10 md:mt-0 m-2">
                     <div className='text-base'>
                         <div>
                             <img className='inline mr-2' src="/phone.svg" alt="phone" />
@@ -38,13 +38,12 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className='mt-8'>
-                        <p className=''>© 2023 Thakur College of Engineering and Technology.</p>
-                        <p className=''>All Rights Reserved.</p>
+                        <p className=''>© 2023 Thakur College of Engineering and Technolog. All Rights Reserved.</p>
                     </div>
                 </div>
 
                 {/* Nav as */}
-                <div className="mt-10 sm:mt-0 w-full sm:w-auto m-2">
+                <div className="mt-10 md:mt-0 w-full md:w-auto m-2">
                     <div className="grid grid-rows gap-2 text-base">
                         <a href="/about">Home</a>
                         <a href="/download">Download</a>
