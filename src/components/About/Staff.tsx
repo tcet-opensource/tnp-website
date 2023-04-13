c1c2e73fe4a5e0b6a3e4c8e31c3b43254a3eaf4c
Rahilsiddique <siddiquerahil19@gmail.com>
2023-04-13 23:07:21 +0530

    [feature] created staffInfo layout
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- src/components/About/Staff.tsx
+++ src/components/About/Staff.tsx
@@ -3,16 +3,18 @@ import type { StaffTNP } from "../../env";
 const Staff: React.FC<{
   data: StaffTNP[];
 }> = ({ data }) => {
-    console.log(data);
-  return <div>{data.map(staff => <StaffPot></StaffPot>)}</div>;
+  return <div className="md:flex grid grid-cols-2 md:justify-between pt-4 flex-wrap gap-2">{data.map(staff => <StaffPot {...staff}/>)}</div>;
 };
 
 export default Staff;
 
-const StaffPot = () => {
+const StaffPot = (staffInfo: StaffTNP) => {
     return (
-        <div>
-            staff info
+        <div className="border border-[#e2e2e2] bg-[#f9fafb] rounded-lg p-4 flex flex-col justify-center items-center gap-2">
+            <img src={"../../../public/Testimonials/"+staffInfo.staffFoto} alt="staffFoto" className="w-52 bg-pink-400"/>
+            <div className="font-semibold">{staffInfo.StaffName}</div>
+            <div>{staffInfo.designation}</div>
+            <div className="bg-[#E4E7EC] px-3 py-2 rounded-lg text-sm">{staffInfo.experties}</div>
         </div>
     )
 }
