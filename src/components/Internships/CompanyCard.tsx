31c9468156d208474a38f31b497c2ab7b4a6976a
adityayadav71 <adityayadav1088@gmail.com>
2023-04-14 17:35:55 +0530

    added CountUpComponent in InternshipHero

--- src/components/Internships/CompanyCard.tsx
+++ src/components/Internships/CompanyCard.tsx
@@ -1,12 +1,11 @@
 type Props = {
-  key: (string & Number) | (number & Number);
   imgLink: String;
   caption: String;
 };
 
-const CompanyCard = ({ key, imgLink, caption }: Props) => {
+const CompanyCard = ({ imgLink, caption }: Props) => {
   return (
-    <div key={key} className="flex flex-col items-center justify-center gap-3">
+    <div className="flex flex-col items-center justify-center gap-3">
       <img src={"/InternshipHero/" + imgLink} alt="company-logo" />
       <p className="lg:text-base text-xs text-center text-[#475467]">{caption}</p>
     </div>
