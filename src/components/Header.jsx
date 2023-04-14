2a0951ee4bc9add112cf0898da5951251d07331d
Anshu Pandey <1032200381@tcetmumbai.in>
2023-04-14 12:35:11 +0530

    TCET text added beside logo (3-build-header)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
 
-export default function Header () {
+export default function HD () {
     return (
         <div>
             <header>
@@ -8,20 +8,22 @@ export default function Header () {
                     <div className="flex flex-wrap justify-between items-center mx-auto">
 
                         <a href="https://tcetmumbai.in" className="flex items-center">
-                            <img src="Images/TCET Logo.png" className="2xl:h-[77px] 2xl:w-[77px] xl:h-[64px] xl:w-[64px] mr-3 2xl:mr-4 md:h-[50px] md:w-[50px] h-[40px] w-[40px]" alt="TCET Logo" />
-                            <img src="Images/Thakur Trust.jpeg" alt="Thakur Trust" srcSet="" className='mr-3 md:-[50px] md:w-[50px] h-[40px] w-[40px] lg:hidden flex'/>
-                            <div className='items-center mr-10 '>   
-                                <p className='hidden lg:flex text-[9px] xl:text-[12px] 2xl:text-[15px] text-[#667085] font-Playfair_Display'><i>Zagdu Singh Charitable Trust`s (Regd.)</i></p>
-                                <p className="hidden lg:flex self-center text-[19px] xl:text-[24px] 2xl:text-[28.8px] font-semibold whitespace-nowrap text-[#DC6803] font-title">Thakur College Of Enginnering & Technology</p>
-                                <p className='hidden lg:flex text-[9px] xl:text-[12px] 2xl:text-[15px] text-[#667085]'>(Autonomous Institute Affiliated to University of Mumbai, Approved by AICTE & <br></br> Govt. of Maharashtra)</p>
+                            <img src="Images/TCET Logo.png" className="2xl:h-[77px] 2xl:w-[77px] xl:h-[64px] xl:w-[64px] mr-3 2xl:mr-4 md:h-[50px] md:w-[50px] h-[30px] w-[30px]" alt="TCET Logo" />
+                            <img src="Images/Thakur Trust.jpeg" alt="Thakur Trust" srcSet="" className='mr-2 md:-[50px] md:w-[50px] h-[30px] w-[30px] lg:hidden flex'/>
+
+                            <div className='items-center xl:mr-10 '>   
+                                <p className='text-[6px] md:text-[9px] xl:text-[12px] 2xl:text-[15px] text-[#667085] font-Playfair_Display'><i>Zagdu Singh Charitable Trust`s (Regd.)</i></p>
+                                <p className="text-[11px] md:text-[19px] xl:text-[24px] 2xl:text-[28.8px] self-center font-semibold whitespace-nowrap text-[#DC6803] font-title">Thakur College Of Enginnering & Technology</p>
+                                <p className='text-[6px] md:text-[9px] xl:text-[12px] 2xl:text-[15px] text-[#667085]'>(Autonomous Institute Affiliated to University of Mumbai, Approved by AICTE & <br></br> Govt. of Maharashtra)</p>
                             </div>  
+
                         </a>
 
                         <div className="flex items-center">
                             <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                                  <span className="sr-only">Open main menu</span>
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                                <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                                <svg className="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             </button>
                         </div>
 
