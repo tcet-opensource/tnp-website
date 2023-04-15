6802e5e9e8f0a09aaec30c95c415d2d58af68e67
adityayadav71 <adityayadav1088@gmail.com>
2023-04-15 20:05:45 +0530

    changed text colors to use tailwind classes

--- src/components/Internships/CompanyCard.tsx
+++ src/components/Internships/CompanyCard.tsx
@@ -7,7 +7,7 @@ const CompanyCard = ({ imgLink, caption }: Props) => {
   return (
     <div className="flex flex-col items-center justify-center gap-3">
       <img src={"/InternshipHero/" + imgLink} alt="company-logo" />
-      <p className="lg:text-base text-xs text-center text-[#475467]">{caption}</p>
+      <p className="lg:text-base text-xs text-center text-slate-600">{caption}</p>
     </div>
   );
 };
