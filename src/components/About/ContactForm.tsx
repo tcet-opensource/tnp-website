7ff0df55afffefb5fb0c84cb6d201b97beafa329
Rahilsiddique <siddiquerahil19@gmail.com>
2023-04-15 18:21:39 +0530

    [fix] fixed tiny issues
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- src/components/About/ContactForm.tsx
+++ src/components/About/ContactForm.tsx
@@ -4,43 +4,44 @@ const ContactForm = () => {
     <form className="flex flex-col gap-8">
       <div className="grid lg:grid-cols-2 gap-4 grid-cols-1">
         <div className="flex flex-col">
-          <label className="text-sm text-[#475467]">First Name</label>
+          <label className="text-sm pb-1 text-slate-600">First Name</label>
           <input
             placeholder="Eg: John Doe"
             type="text"
-            className="h-11 bg-[#f9fafb] rounded-lg p-3"
+            className="h-11 bg-gray-100 border-2 border-gray-200 rounded-lg p-3"
           />
         </div>
         <div className="flex flex-col">
-          <label className="text-sm text-[#475467]">Last Name</label>
+          <label className="text-sm pb-1 text-slate-600 ">Last Name</label>
           <input
             type="text"
-            className="h-11 bg-[#f9fafb] rounded-lg p-3"
+            className="h-11 bg-gray-100 border-2 border-gray-200 rounded-lg p-3"
             placeholder="yeah"
           />
         </div>
       </div>
       <div className="flex flex-col">
-        <label className="text-sm text-[#475467]">Email</label>
+        <label className="text-sm pb-1 text-slate-600 ">Email</label>
         <input
           type="email"
-          className="h-11 bg-[#f9fafb] rounded-lg p-3"
-          placeholder="yeah"
+          className="h-11 bg-gray-100 border-2 border-gray-200 rounded-lg p-3 placeholder:underline"
+          placeholder="johnDoe@gmail.com"
         />
       </div>
       <div className="flex flex-col">
-        <label className="text-sm text-[#475467]">Message</label>
+        <label className="text-sm pb-1 text-slate-600 ">Message</label>
         <textarea
           name="message"
           id="text-area"
           cols={15}
           rows={5}
-          className="bg-[#f9fafb] rounded-lg p-3"
+          className="bg-gray-100 border-2 border-gray-200 rounded-lg p-3"
+          placeholder="Enter your message"
         />
       </div>
       <div className="flex flex-grow h-12 pt-3">
         <button
-          className="flex flex-grow justify-center items-center bg-[#1570EF] rounded-lg p-3text-white text-white text-xl"
+          className="flex flex-grow justify-center items-center bg-blue-600 rounded-lg p-3text-white text-white text-base"
           placeholder="message"
         >
           Submit
--- src/components/About/Staff.tsx
+++ src/components/About/Staff.tsx
@@ -11,10 +11,10 @@ export default Staff;
 const StaffPost = (staffInfo: StaffTNP) => {
     return (
         <div className="border border-[#e2e2e2] bg-[#f9fafb] rounded-lg p-4 flex flex-col justify-center items-center gap-2">
-            <img src={"../../../public/Testimonials/"+staffInfo.staffPhoto} alt="staffFoto" className="w-52 bg-pink-400"/>
+            <img src={"/Testimonials/"+staffInfo.staffPhoto} alt="staffFoto" className="w-52 rounded-xl"/>
             <div className="font-semibold">{staffInfo.StaffName}</div>
             <div>{staffInfo.designation}</div>
-            <div className="bg-[#E4E7EC] px-3 py-2 rounded-lg text-sm">{staffInfo.experties}</div>
+            <div className="bg-[#E4E7EC] px-3 py-2 rounded-lg text-sm w-full text-slate-600 text-center">{staffInfo.experties}</div>
         </div>
     )
 }
