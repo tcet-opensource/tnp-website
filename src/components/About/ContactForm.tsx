e4ba001858b8798fed0606572aeb96f154e0d5aa
JayeshVP24 <jayeshpotlabattini@gmail.com>
2023-04-15 19:05:04 +0530

    [FIX] About TnP page fixed

--- src/components/About/ContactForm.tsx
+++ src/components/About/ContactForm.tsx
@@ -1,52 +1,52 @@
 import "../../../styles.css";
 const ContactForm = () => {
   return (
-    <form className="flex flex-col gap-8">
+    <form className="flex flex-col gap-4">
       <div className="grid lg:grid-cols-2 gap-4 grid-cols-1">
-        <div className="flex flex-col">
+        <span className="flex flex-col">
           <label className="text-sm pb-1 text-slate-600">First Name</label>
           <input
             placeholder="Eg: John Doe"
             type="text"
-            className="h-11 bg-gray-100 border-2 border-gray-200 rounded-lg p-3"
+            className="h-11 bg-gray-50 border border-gray-200 rounded-lg p-3"
           />
-        </div>
-        <div className="flex flex-col">
+        </span>
+        <span className="flex flex-col">
           <label className="text-sm pb-1 text-slate-600 ">Last Name</label>
           <input
             type="text"
-            className="h-11 bg-gray-100 border-2 border-gray-200 rounded-lg p-3"
+            className="h-11 bg-gray-50 border border-gray-200 rounded-lg p-3"
             placeholder="yeah"
           />
-        </div>
+        </span>
       </div>
-      <div className="flex flex-col">
+      <span className="flex flex-col">
         <label className="text-sm pb-1 text-slate-600 ">Email</label>
         <input
           type="email"
-          className="h-11 bg-gray-100 border-2 border-gray-200 rounded-lg p-3 placeholder:underline"
+          className="h-11 bg-gray-50 border border-gray-200 rounded-lg p-3 placeholder:underline"
           placeholder="johnDoe@gmail.com"
         />
-      </div>
-      <div className="flex flex-col">
+      </span>
+      <span className="flex flex-col">
         <label className="text-sm pb-1 text-slate-600 ">Message</label>
         <textarea
           name="message"
           id="text-area"
           cols={15}
           rows={5}
-          className="bg-gray-100 border-2 border-gray-200 rounded-lg p-3"
+          className="bg-gray-50 border border-gray-200 rounded-lg p-3"
           placeholder="Enter your message"
         />
-      </div>
-      <div className="flex flex-grow h-12 pt-3">
-        <button
-          className="flex flex-grow justify-center items-center bg-blue-600 rounded-lg p-3text-white text-white text-base"
+      </span>
+      <button
+          className="flex flex-grow justify-center items-center bg-blue-500 rounded-lg py-2 text-white text-base
+          hover:bg-blue-400 mt-2"
           placeholder="message"
+          type="submit"
         >
           Submit
         </button>
-      </div>
     </form>
   );
 };
