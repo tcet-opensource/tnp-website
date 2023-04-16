f66439435f9eb2882d967eaa166eb5e1638c3468
Shudhanshu Singh <shudhanshusingh051@gmail.com>
2023-04-16 15:20:50 +0530

    Cleaning of previous code

--- src/components/IndustryInteraction/IndustryInteraction.jsx
+++ src/components/IndustryInteraction/IndustryInteraction.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigation, Pagination, Scrollbar, A11y, FreeMode } from "swiper";
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -8,103 +8,89 @@ import "swiper/css/scrollbar";
 import "swiper/css/free-mode";
 
 function IndustryInteraction() {
+  const IndustryInteractionData = [
+    {
+      companyLogo:
+        "https://jobs.vibhaga.com/wp-content/uploads/2018/07/infosys-logo.jpg",
+      companyName: "Infosys",
+      themeColor: "blue",
+      description:
+        "The Training and Placement department serves as a bridge between the academic environment and the industry by establishing connections with various companies and organisations. The ultimate aim of the T&P department is to ensure that students are well prepared for the job market and have access to the resources they need to achieve their career goals.",
+      eventImages: [
+        "https://i0.wp.com/www.blogs.hss.ed.ac.uk/pubs-and-publications/files/2018/04/conference-meeting-seminar.jpg?w=1352",
+        "https://mk0blogcolonialjxv0v.kinstacdn.com/wp-content/uploads/2020/07/meeting.jpg",
+      ],
+      imageLeftAlign: false,
+    },
+    {
+      companyLogo:
+        "https://i1.wp.com/www.itvoice.in/wp-content/uploads/2016/12/Reliance_Jio_Logo_October_2015.png?ssl=1",
+      companyName: "Jio",
+      themeColor: "red",
+      description:
+        "The Training and Placement department serves as a bridge between the academic environment and the industry by establishing connections with various companies and organisations. The ultimate aim of the T&P department is to ensure that students are well prepared for the job market and have access to the resources they need to achieve their career goals.",
+      eventImages: [
+        "https://mk0blogcolonialjxv0v.kinstacdn.com/wp-content/uploads/2020/07/meeting.jpg",
+        "https://i0.wp.com/www.blogs.hss.ed.ac.uk/pubs-and-publications/files/2018/04/conference-meeting-seminar.jpg?w=1352",
+      ],
+      imageLeftAlign: true,
+    },
+  ];
   return (
     <div>
-      <div className="w-full m-4 h-fit bg-blue-100 p-10 rounded-xl  xl:flex  xl:flex-row md:flex-row sm:flex-col">
-        <div className="flex-0.5 flex flex-col h-full mb-4 align-middle p-5 ">
-          <img
-            className="rounded-full w-12 mb-4 mt-4"
-            src="https://jobs.vibhaga.com/wp-content/uploads/2018/07/infosys-logo.jpg"
-          />
-          <p className="text-3xl font-title mb-4">
-            Infosys Industry Interaction
-          </p>
-          <p className="xl:w-4/5 md:w-4/5s">
-            The Training and Placement department serves as a bridge between the
-            academic environment and the industry by establishing connections
-            with various companies and organisations. The ultimate aim of the
-            T&P department is to ensure that students are well prepared for the
-            job market and have access to the resources they need to achieve
-            their career goals.
-          </p>
-        </div>
-        <div className="h-full flex justify-center align-middle xl:w-1/2 md:1/2 sm:w-full">
-          <div className="w-full">
-            <Swiper
-              modules={[Navigation, Pagination, Scrollbar, A11y]}
-              spaceBetween={50}
-              slidesPerView={1}
-              pagination={{ clickable: true }}
-              onSlideChange={() => console.log("slide change")}
-              onSwiper={(swiper) => console.log(swiper)}
-            >
-              <SwiperSlide>
-                <div className="w-full mb-10 mt-4">
-                  <img
-                    className="rounded-xl  w-full object-contain"
-                    src="https://live.staticflickr.com/761/23388109362_49c91ba9f6.jpg"
-                  />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="w-full mb-10 mt-4">
-                  <img
-                    className="rounded-xl  w-full object-contain"
-                    src="https://live.staticflickr.com/761/23388109362_49c91ba9f6.jpg"
-                  />
-                </div>
-              </SwiperSlide>
-            </Swiper>
+      <div>
+      {IndustryInteractionData.map((data) => (
+        <div
+          className={`w-full m-4 h-fit bg-${
+            data.themeColor
+          }-100 p-10 rounded-xl  xl:flex ${
+            data.imageLeftAlign
+              ? "xl:flex-row-reverse md:flex-row-reverse"
+              : "xl:flex-row md:flex-row"
+          } sm:flex-col`}
+        >
+          <div className={`flex flex-col h-full mb-4 align-middle p-5 ${data.imageLeftAlign ? "xl:ml-6 md:ml-6":"xl:ml-0 md:ml-0"} sm:ml-0 xl:w-1/2 md:1/2 sm:w-full`}>
+            <img
+              className="rounded-full w-12 mb-4 mt-4"
+              src={data.companyLogo}
+            />
+            <p className="text-3xl font-title mb-4">
+              {data.companyName} Industry Interaction
+            </p>
+            <p className="xl:w-4/5 md:w-4/5 sm:w-full">
+              {data.description}
+            </p>
+          </div>
+          <div className="h-full flex justify-center align-middle xl:w-1/2 md:1/2 sm:w-full">
+            <div className="w-full">
+              <Swiper
+                modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+                spaceBetween={50}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+                onSlideChange={() => console.log("slide change")}
+                onSwiper={(swiper) => console.log(swiper)}
+                style={{"--swiper-pagination-color": "#344054"}}
+              >
+                {data.eventImages.map((imgLink) => (
+                  <SwiperSlide>
+                    <div className="w-full mb-10 mt-4">
+                      <img
+                        className="rounded-xl  w-full object-contain"
+                        src={imgLink}
+                      />
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="w-full m-4 h-fit bg-red-100 p-10 rounded-xl  xl:flex  xl:flex-row-reverse md:flex-row sm:flex-col">
-        <div className="flex flex-col h-full mb-4 align-middle justify-center p-5 xl:ml-6 md:ml-6 sm:ml-0">
-          <img
-            className="rounded-full w-12 mb-4 mt-4"
-            src="https://i1.wp.com/www.itvoice.in/wp-content/uploads/2016/12/Reliance_Jio_Logo_October_2015.png?ssl=1"
-          />
-          <p className="text-3xl font-title mb-4">
-            Jio Industry Interaction
-          </p>
-          <p className="xl:w-4/5 md:w-4/5">
-            The Training and Placement department serves as a bridge between the
-            academic environment and the industry by establishing connections
-            with various companies and organisations. The ultimate aim of the
-            T&P department is to ensure that students are well prepared for the
-            job market and have access to the resources they need to achieve
-            their career goals.
-          </p>
-        </div>
-        <div className="h-full flex justify-center align-middle xl:w-1/2 md:1/2 sm:w-full">
-          <div className="w-full">
-            <Swiper
-              modules={[Navigation, Pagination, Scrollbar, A11y]}
-              spaceBetween={50}
-              slidesPerView={1}
-              pagination={{ clickable: true }}
-              onSlideChange={() => console.log("slide change")}
-              onSwiper={(swiper) => console.log(swiper)}
-            >
-              <SwiperSlide>
-                <div className="w-full mb-10 mt-4">
-                  <img
-                    className="rounded-xl  w-full object-contain"
-                    src="https://live.staticflickr.com/761/23388109362_49c91ba9f6.jpg"
-                  />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="w-full mb-10 mt-4">
-                  <img
-                    className="rounded-xl  w-full object-contain"
-                    src="https://live.staticflickr.com/761/23388109362_49c91ba9f6.jpg"
-                  />
-                </div>
-              </SwiperSlide>
-            </Swiper>
-          </div>
-        </div>
+      ))}
       </div>
     </div>
   );
