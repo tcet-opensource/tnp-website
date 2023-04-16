7c2114ac852cf0b385e2a3e60b05638662b12ecc
Shudhanshu Singh <shudhanshusingh051@gmail.com>
2023-04-16 16:17:48 +0530

    Industry Interaction Header fixed

--- src/components/IndustryInteraction/IndustryInteraction.jsx
+++ src/components/IndustryInteraction/IndustryInteraction.jsx
@@ -8,6 +8,34 @@ import "swiper/css/scrollbar";
 import "swiper/css/free-mode";
 
 function IndustryInteraction() {
+  const IndustryInteractionHeaderData = [
+    {
+      companyLogo:
+        "https://jobs.vibhaga.com/wp-content/uploads/2018/07/infosys-logo.jpg",
+      companyName: "Infosys",
+      themeColor: "blue",
+      description:
+        "The Training and Placement department serves as a bridge between the academic environment and the industry by establishing connections with various companies and organisations. The ultimate aim of the T&P department is to ensure that students are well prepared for the job market and have access to the resources they need to achieve their career goals.",
+      eventImages: [
+        "https://i0.wp.com/www.blogs.hss.ed.ac.uk/pubs-and-publications/files/2018/04/conference-meeting-seminar.jpg?w=1352",
+        "https://mk0blogcolonialjxv0v.kinstacdn.com/wp-content/uploads/2020/07/meeting.jpg",
+      ],
+      imageLeftAlign: false,
+    },
+    {
+      companyLogo:
+        "https://i1.wp.com/www.itvoice.in/wp-content/uploads/2016/12/Reliance_Jio_Logo_October_2015.png?ssl=1",
+      companyName: "Jio",
+      themeColor: "red",
+      description:
+        "The Training and Placement department serves as a bridge between the academic environment and the industry by establishing connections with various companies and organisations. The ultimate aim of the T&P department is to ensure that students are well prepared for the job market and have access to the resources they need to achieve their career goals.",
+      eventImages: [
+        "https://mk0blogcolonialjxv0v.kinstacdn.com/wp-content/uploads/2020/07/meeting.jpg",
+        "https://i0.wp.com/www.blogs.hss.ed.ac.uk/pubs-and-publications/files/2018/04/conference-meeting-seminar.jpg?w=1352",
+      ],
+      imageLeftAlign: true,
+    },
+  ];
   const IndustryInteractionData = [
     {
       companyLogo:
@@ -38,8 +66,8 @@ function IndustryInteraction() {
   ];
   return (
     <div>
-      <div className="h-screen w-full m-4 flex">
-        <div className="w-2/4 h-full mt-10">
+      <div className="w-full m-4 flex xl:flex-row flex-col">
+        <div className="h-full mt-10 xl:w-2/4 sm:w-full p-3">
           <p className="text-4xl font-title mb-4">Industry Interaction</p>
           <p>
             The Training and Placement department serves as a bridge between the
@@ -50,42 +78,30 @@ function IndustryInteraction() {
             their career goals.
           </p>
         </div>
-        <div className="w-full flex justify-center align-middle">
-          <div className="w-1/4 border-2 h-fit border-blue-400 rounded-2xl p-4 bg-blue-100 m-6">
-            <img
-              src="https://jobs.vibhaga.com/wp-content/uploads/2018/07/infosys-logo.jpg"
-              className="rounded-full w-12"
-            />
-            <p className="font-title mb-3 mt-3 ">Infosys</p>
-            <p className="text-gray-500 font-light">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehender
-            </p>
-            <button className="w-full bg-white rounded-lg p-2 mt-4 hover:bg-blue-400 hover:text-white">
-              Read More ↓
-            </button>
-          </div>
-          <div className="w-1/4 border-2 h-fit border-red-400 rounded-2xl p-4 bg-red-100 m-6 mt-24">
-            <img
-              src="https://jobs.vibhaga.com/wp-content/uploads/2018/07/infosys-logo.jpg"
-              className="rounded-full w-12"
-            />
-            <p className="font-title mb-3 mt-3 ">Infosys</p>
-            <p className="text-gray-500 font-light ">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehender
-            </p>
-            <button className="w-full bg-white rounded-lg p-2 mt-4 hover:bg-blue-400 hover:text-white">
-              Read More ↓
-            </button>
-          </div>
-          
+        <div className="w-full flex justify-center align-middle flex-col xl:flex-row">
+          {IndustryInteractionHeaderData.map((data, i) => (
+            <div
+              key={i}
+              className={`border-2 h-fit border-${
+                data.themeColor
+              }-400 rounded-2xl p-4 bg-${
+                data.themeColor
+              }-100 m-6 xl:w-1/4 sm:w-full ${
+                i % 2 != 0 ? "xl:mt-24 md:mt-24 sm:mt-6" : ""
+              }`}
+            >
+              <img src={data.companyLogo} className="rounded-full w-12" />
+              <p className="font-title mb-3 mt-3 ">{data.companyName}</p>
+              <p className="text-gray-500 font-light text-sm">
+                {data.description}
+              </p>
+              <button
+                className={`w-full bg-white rounded-lg p-2 mt-4 hover:bg-${data.themeColor}-400 hover:text-white`}
+              >
+                Read More ↓
+              </button>
+            </div>
+          ))}
         </div>
       </div>
       <div>
